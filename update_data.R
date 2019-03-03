807cfadbbfc669a17f03905bef670a091107baba
andersfi <anders.finstad@ntnu.no>
2019-03-03 18:06:09 +0100

    removing auto-commit

--- update_data.R
+++ update_data.R
@@ -10,12 +10,12 @@ source("./R/2_normalize_data.R")
 source("./R/3_visulize_occurrences.R")
 
 #----------------
-# push chances 
+# push chances - work in progress... update github repro manually for now
 #--------------------
 
 # Push to github to sync eventID after updating
 # Assumes installed and linked to correct repro 
 # Remember, set ssh key first: see https://happygitwithr.com/ssh-keys.html#ssh-keys
-system("git add .")
-system("git commit -m 'updating lokal data-cache'")
-system("git push origin master")
+# system("git add .")
+# system("git commit -m 'updating lokal data-cache'")
+# system("git push origin master")
