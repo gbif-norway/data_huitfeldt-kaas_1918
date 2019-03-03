c11e274fa4487c366e4ebbd6034f66bc83b475e4
andersfi <anders.finstad@ntnu.no>
2019-03-03 16:52:31 +0100

    updating lokal data-cache

--- R/update_data.R
+++ R/update_data.R
@@ -7,6 +7,7 @@ source("./R/2_normalize_data.R")
 source("./R/3_visulize_occurrences.R")
 
 # Push to github to sync eventID after updating
+# Assumes installed and linked to correct repro 
 # Remember, set ssh key first: see https://happygitwithr.com/ssh-keys.html#ssh-keys
 system("git add .")
 system("git commit -m 'updating lokal data-cache'")
