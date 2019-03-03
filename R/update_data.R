a355e7176889c7b471719de8e170574ec8d22d99
andersfi <anders.finstad@ntnu.no>
2019-03-03 16:45:36 +0100

    updating lokal data-cache

--- R/update_data.R
+++ R/update_data.R
@@ -7,16 +7,7 @@ source("./R/2_normalize_data.R")
 source("./R/3_visulize_occurrences.R")
 
 # Push to github to sync eventID after updating
-#system("git add .")
-#system("git commit -m 'updating lokal data-cache'")
-#system("git push origin master")
-
-# Setup to automate pushing. From: https://stackoverflow.com/questions/8588768/how-do-i-avoid-the-specification-of-the-username-and-password-at-every-git-push
-
-# sudo apt-get install libgnome-keyring-dev
-# sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
-# git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
-# 
-# git config --global credential.helper store
-
-# https://git@github.com/username/reponame.git
+# Remember, set ssh key first: see https://happygitwithr.com/ssh-keys.html#ssh-keys
+system("git add .")
+system("git commit -m 'updating lokal data-cache'")
+system("git push origin master")
